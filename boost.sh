df5b0a33d89d0962177208e2fb76db7e82e03d22
Stephan T. Lavavej <stl@nuwen.net>
2012-07-05 01:24:04 -0700

    Distro 9.2.
    boost-BOOST_THREAD_USE_LIB.patch:
        Regenerate.
    
    boost.sh:
        Pipe the packaging command to NUL, which speeds up the build.
        Remove the sleep-then-reclean hack, which shouldn't be necessary anymore.

--- boost.sh
+++ boost.sh
@@ -5,14 +5,14 @@
 
 source 0_append_distro_path.sh
 
-7za x '-oC:\Temp\gcc' boost_1_49_0.tar > NUL || { echo boost_1_49_0.tar - EPIC FAIL ; exit 1; }
+7za x '-oC:\Temp\gcc' boost_1_50_0.tar > NUL || { echo boost_1_50_0.tar - EPIC FAIL ; exit 1; }
 
-patch -d /c/temp/gcc/boost_1_49_0 -p1 < boost-BOOST_THREAD_USE_LIB.patch
-patch -d /c/temp/gcc/boost_1_49_0 -p1 < boost-bootstrap.patch
-patch -d /c/temp/gcc/boost_1_49_0 -p1 < boost-c++11.patch
+patch -d /c/temp/gcc/boost_1_50_0 -p1 < boost-BOOST_THREAD_USE_LIB.patch
+patch -d /c/temp/gcc/boost_1_50_0 -p1 < boost-bootstrap.patch
+patch -d /c/temp/gcc/boost_1_50_0 -p1 < boost-c++11.patch
 
 cd /c/temp/gcc
-mv boost_1_49_0 src
+mv boost_1_50_0 src
 mkdir -p dest/include
 cd src
 bootstrap.sh || { echo boost - EPIC FAIL ; exit 1; }
@@ -22,13 +22,16 @@ cd /c/temp/gcc/dest/lib
 for i in *.a; do mv $i ${i%-mgw*.a}.a; done
 cd /c/temp/gcc
 mv src/boost dest/include
-mv dest boost-1.49.0
+mv dest boost-1.50.0
 
-cd boost-1.49.0
-7za -mx0 a ../boost-1.49.0.7z *
+echo Packaging...
 
-# Annoying!
+cd boost-1.50.0
+7za -mx0 a ../boost-1.50.0.7z * > NUL || { echo boost-1.50.0.7z - EPIC FAIL ; exit 1; }
+
+echo Cleaning...
+
 cd /c/temp/gcc
 rm -rf src
-sleep 45
-rm -rf src
+
+echo Done.
