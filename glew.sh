4aa4aede2468bf2a869983fb126370e3c545f8f0
Stephan T. Lavavej <stl@nuwen.net>
2013-09-08 18:10:08 -0700

    Distro 11.2.
    glew.sh,
    glew-core.patch:
        Patch GLEW to work with OpenGL core profiles.

--- glew.sh
+++ glew.sh
@@ -5,6 +5,7 @@ source 0_append_distro_path.sh
 7z x '-oC:\Temp\gcc' glew-1.10.0.tar > NUL || fail_with glew-1.10.0.tar - EPIC FAIL
 
 patch -d /c/temp/gcc/glew-1.10.0 -p1 < glew.patch
+patch -d /c/temp/gcc/glew-1.10.0 -p1 < glew-core.patch
 
 cd /c/temp/gcc
 mv glew-1.10.0 src
