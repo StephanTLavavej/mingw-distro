01c032869cd240e7ed453c5a886899cb7d3871b6
Stephan T. Lavavej <stl@nuwen.net>
2013-11-25 16:32:35 -0800

    Distro 11.4.
    freetype.sh:
        Delete spurious PaxHeaders.18661 directory.
        Stop performing directory surgery.
    
    grep+lame+make+sed+wget.sh:
        Follow "build_w32.bat gcc" because configure isn't supported for MinGW.
        Note that config.h.W32 defines PATH_SEPARATOR_CHAR correctly.
        Note that NLS is disabled by default.
        Note that we still need to request case-insensitivity.
    
    make-mingw-w64.patch:
        First: http://git.savannah.gnu.org/cgit/make.git/commit/?id=d1d737d
        Then "%Id" is non-Standard, so change it to "%lld".
        Also "0x%x" is WRONG, so change it to "0x%llx".

--- freetype.sh
+++ freetype.sh
@@ -2,22 +2,18 @@
 
 source 0_append_distro_path.sh
 
-7z x '-oC:\Temp\gcc' freetype-2.5.0.1.tar > NUL || fail_with freetype-2.5.0.1.tar - EPIC FAIL
+7z x '-oC:\Temp\gcc' freetype-2.5.1.tar > NUL || fail_with freetype-2.5.1.tar - EPIC FAIL
 
 cd /c/temp/gcc
-mv freetype-2.5.0.1 src
+mv freetype-2.5.1 src
 mkdir build dest
 cd build
 ../src/configure --disable-shared --prefix=/c/temp/gcc/dest "CFLAGS=-s -O3" --without-png || fail_with freetype - EPIC FAIL
 make all install || fail_with freetype - EPIC FAIL
 cd /c/temp/gcc
-rm -rf build src
-
-mv dest freetype-2.5.0.1
-cd freetype-2.5.0.1
+rm -rf build src PaxHeaders.18661
+mv dest freetype-2.5.1
+cd freetype-2.5.1
 rm -rf bin lib/pkgconfig lib/*.la share
 
-mv include/freetype2/freetype include
-rmdir include/freetype2
-
-7z -mx0 a ../freetype-2.5.0.1.7z *
+7z -mx0 a ../freetype-2.5.1.7z *
--- grep+lame+make+sed+wget.sh
+++ grep+lame+make+sed+wget.sh
@@ -9,6 +9,7 @@ source 0_append_distro_path.sh
 7z x '-oC:\Temp\gcc' wget-1.14.tar > NUL || fail_with wget-1.14.tar - EPIC FAIL
 
 patch -d /c/temp/gcc/grep-2.10 -p1 < grep.patch
+patch -d /c/temp/gcc/make-4.0 -p1 < make-mingw-w64.patch
 patch -d /c/temp/gcc/sed-4.2.2 -p1 < sed.patch
 patch -d /c/temp/gcc/wget-1.14 -p1 < wget.patch
 
@@ -33,16 +34,28 @@ mv frontend/lame.exe ../dest/bin
 cd /c/temp/gcc
 rm -rf build src
 
+
+# Distilled from "build_w32.bat gcc".
+# http://lists.gnu.org/archive/html/make-w32/2013-10/msg00029.html
+# Eli Zaretskii> The only way to build a MinGW Make that is officially supported is build_w32.bat.
+
 mv make-4.0 src
-mkdir build
-cd build
-../src/configure --prefix=/c/temp/gcc/dest --enable-case-insensitive-file-system --disable-nls || fail_with make - EPIC FAIL
-sed -e "s/#define PATH_SEPARATOR_CHAR ':'/#define PATH_SEPARATOR_CHAR ';'/" config.h > config.fixed
-mv -f config.fixed config.h
-make "CFLAGS=-O3" "LDFLAGS=-s" || fail_with make - EPIC FAIL
-mv make.exe ../dest/bin
+cd src
+mv config.h.W32 config.h
+
+gcc -Wall -Wextra -Werror -s -O3 \
+-Wno-parentheses -Wno-sign-compare -Wno-unused-but-set-variable -Wno-unused-label -Wno-unused-parameter \
+-DHAVE_CONFIG_H -DWINDOWS32 -I. -Iglob -Iw32/include -Iw32/subproc \
+-DHAVE_CASE_INSENSITIVE_FS \
+ar.c arscan.c commands.c default.c dir.c expand.c file.c function.c getloadavg.c getopt.c getopt1.c \
+hash.c implicit.c job.c load.c loadapi.c main.c misc.c output.c read.c remake.c remote-stub.c rule.c \
+signame.c strcache.c variable.c version.c vpath.c glob/fnmatch.c glob/glob.c w32/pathstuff.c \
+w32/compat/posixfcn.c w32/subproc/misc.c w32/subproc/sub_proc.c w32/subproc/w32err.c \
+-o ../dest/bin/make.exe || fail_with make - EPIC FAIL
+
 cd /c/temp/gcc
-rm -rf build src
+rm -rf src
+
 
 mv sed-4.2.2 src
 mkdir build
