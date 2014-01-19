06e118ac171bf991d82b0de2f6fde1ffc367b9d4
Stephan T. Lavavej <stl@nuwen.net>
2014-01-19 02:35:19 -0800

    Distro 11.6.
    gdb.sh:
        Add. Note that setting build/host/target is necessary.
    
    pcre.sh,
    pcre-color.patch:
        Add color support to pcregrep.

---
+++ gdb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source 0_append_distro_path.sh
+
+7z x '-oC:\Temp\gcc' gdb-7.6.2.tar > NUL || fail_with gdb-7.6.2.tar - EPIC FAIL
+
+cd /c/temp/gcc
+mv gdb-7.6.2 src
+mkdir build dest
+cd build
+../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --prefix=/c/temp/gcc/dest --disable-nls || fail_with gdb - EPIC FAIL
+make all install "CFLAGS=-O3" "LDFLAGS=-s" || fail_with gdb - EPIC FAIL
+cd /c/temp/gcc
+rm -rf build src
+mv dest gdb-7.6.2
+cd gdb-7.6.2
+rm -rf include lib share
+
+7z -mx0 a ../gdb-7.6.2.7z *
--- pcre.sh
+++ pcre.sh
@@ -5,6 +5,7 @@ source 0_append_distro_path.sh
 7z x '-oC:\Temp\gcc' pcre-8.34.tar > NUL || fail_with pcre-8.34.tar - EPIC FAIL
 
 patch -d /c/temp/gcc/pcre-8.34 -p1 < pcre.patch
+patch -d /c/temp/gcc/pcre-8.34 -p1 < pcre-color.patch
 
 cd /c/temp/gcc
 mv pcre-8.34 src
@@ -16,7 +17,7 @@ cd /c/temp/gcc
 rm -rf build src
 mv dest pcre-8.34
 cd pcre-8.34
-rm -rf bin lib/pkgconfig lib/*.la share
+rm -rf bin/pcre-config lib/pkgconfig lib/*.la share
 cp include/pcreposix.h include/regex.h
 
 7z -mx0 a ../pcre-8.34.7z *
