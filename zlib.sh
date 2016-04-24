9690048b0a78cf2f2ec037afdbb91c71fdba2137
Stephan T. Lavavej <stl@nuwen.net>
2016-04-23 17:41:45 -0700

    Build zlib with -DTOO_FAR=32767 instead of patching. Fixes #11.

--- zlib.sh
+++ zlib.sh
@@ -4,13 +4,12 @@ source ./0_append_distro_path.sh
 
 extract_file zlib-1.2.8.tar
 
-patch -d /c/temp/gcc/zlib-1.2.8 -p1 < zlib.patch
-
 cd /c/temp/gcc
 mv zlib-1.2.8 src
 mkdir -p dest/include dest/lib
 cd src
-gcc -s -O3 -c *.c || fail_with zlib 1 - EPIC FAIL
+# -DTOO_FAR=32767 : http://optipng.sourceforge.net/pngtech/too_far.html
+gcc -DTOO_FAR=32767 -s -O3 -c *.c || fail_with zlib 1 - EPIC FAIL
 ar rs ../dest/lib/libz.a *.o
 mv zconf.h zlib.h ../dest/include
 cd /c/temp/gcc
