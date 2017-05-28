b55e1b1b0c460b71c5ce4b200a0e30827ddc331f
Stephan T. Lavavej <stl@nuwen.net>
2017-05-28 16:59:45 -0700

    zlib 1.2.11.

--- zlib.sh
+++ zlib.sh
@@ -2,10 +2,10 @@
 
 source ./0_append_distro_path.sh
 
-extract_file zlib-1.2.8.tar
+extract_file zlib-1.2.11.tar
 
 cd /c/temp/gcc
-mv zlib-1.2.8 src
+mv zlib-1.2.11 src
 mkdir -p dest/include dest/lib
 cd src
 # -DTOO_FAR=32767 : http://optipng.sourceforge.net/pngtech/too_far.html
@@ -14,7 +14,7 @@ ar rs ../dest/lib/libz.a *.o || fail_with zlib 2 - EPIC FAIL
 mv zconf.h zlib.h ../dest/include
 cd /c/temp/gcc
 rm -rf src
-mv dest zlib-1.2.8
-cd zlib-1.2.8
+mv dest zlib-1.2.11
+cd zlib-1.2.11
 
-7z -mx0 a ../zlib-1.2.8.7z *
+7z -mx0 a ../zlib-1.2.11.7z *
