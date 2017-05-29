d4c83fab0a72f06cafd5f0a6b32f1f396f83ff2e
Stephan T. Lavavej <stl@nuwen.net>
2017-05-28 18:16:46 -0700

    PCRE 8.40.
    Regenerate pcre-color.patch.

--- pcre.sh
+++ pcre.sh
@@ -2,12 +2,12 @@
 
 source ./0_append_distro_path.sh
 
-extract_file pcre-8.39.tar
+extract_file pcre-8.40.tar
 
-patch -d /c/temp/gcc/pcre-8.39 -p1 < pcre-color.patch
+patch -d /c/temp/gcc/pcre-8.40 -p1 < pcre-color.patch
 
 cd /c/temp/gcc
-mv pcre-8.39 src
+mv pcre-8.40 src
 mkdir build dest
 cd build
 
@@ -19,9 +19,9 @@ make $X_MAKE_JOBS all || fail_with pcre 2 - EPIC FAIL
 make install || fail_with pcre 3 - EPIC FAIL
 cd /c/temp/gcc
 rm -rf build src
-mv dest pcre-8.39
-cd pcre-8.39
+mv dest pcre-8.40
+cd pcre-8.40
 rm -rf bin/pcre-config lib/pkgconfig lib/*.la share
 cp include/pcreposix.h include/regex.h
 
-7z -mx0 a ../pcre-8.39.7z *
+7z -mx0 a ../pcre-8.40.7z *
