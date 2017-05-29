c47483d216da8bc264da61e821520b9a5e68c6c8
Stephan T. Lavavej <stl@nuwen.net>
2017-05-28 18:34:16 -0700

    GLM 0.9.8.4.
    The zip contains an unversioned directory yet again.

--- glm.sh
+++ glm.sh
@@ -2,14 +2,14 @@
 
 source ./0_append_distro_path.sh
 
-extract_file glm-0.9.8.3.zip
+extract_file glm-0.9.8.4.zip
 
 cd /c/temp/gcc
-mv glm-0.9.8.3 src
+mv glm src
 mkdir -p dest/include
 mv src/glm dest/include
 rm -rf src
-mv dest glm-0.9.8.3
-cd glm-0.9.8.3
+mv dest glm-0.9.8.4
+cd glm-0.9.8.4
 
-7z -mx0 a ../glm-0.9.8.3.7z *
+7z -mx0 a ../glm-0.9.8.4.7z *
