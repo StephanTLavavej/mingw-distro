0ad9e1bbf7f56dc2e1ac4a5298a71391a093d7c1
Stephan T. Lavavej <stl@nuwen.net>
2017-07-30 17:53:17 -0700

    grep 3.1 fixes #6.
    grep-configure.patch:
        Activating WINDOWS_STAT_INODES fixes recursion.
    
    grep-lock.patch:
        We still need to use SetConsoleCtrlHandler() to avoid leaking color.
    
    grep.patch:
        Everything here has either been fixed upstream or decomposed into smaller patches.
        The distro's batch file needs `set GREP_COLORS=mt=01;32:fn=36:ln=33:bn=31:se=35`.
        It also needs a Doskey macro for `--color=auto`.
    
    grep.sh:
        --disable-largefile was added in distro 11.0.
        It was still necessary for grep 2.10, but not for grep 3.1.

--- grep.sh
+++ grep.sh
@@ -2,19 +2,20 @@
 
 source ./0_append_distro_path.sh
 
-extract_file grep-2.10.tar
+extract_file grep-3.1.tar
 
-patch -d /c/temp/gcc/grep-2.10 -p1 < grep.patch
+patch -d /c/temp/gcc/grep-3.1 -p1 < grep-configure.patch
+patch -d /c/temp/gcc/grep-3.1 -p1 < grep-lock.patch
 
 cd /c/temp/gcc
 mkdir -p dest/bin
 
-mv grep-2.10 src
+mv grep-3.1 src
 mkdir build
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=/c/temp/gcc/dest --disable-nls --disable-largefile "CFLAGS=-s -O3 -DPCRE_STATIC" \
+--prefix=/c/temp/gcc/dest --disable-nls "CFLAGS=-s -O3 -DPCRE_STATIC" \
 || fail_with grep 1 - EPIC FAIL
 
 make $X_MAKE_JOBS || fail_with grep 2 - EPIC FAIL
@@ -22,7 +23,7 @@ mv src/grep.exe ../dest/bin || fail_with grep 3 - EPIC FAIL
 cd /c/temp/gcc
 rm -rf build src
 
-mv dest grep-2.10
-cd grep-2.10
+mv dest grep-3.1
+cd grep-3.1
 
-7z -mx0 a ../grep-2.10.7z *
+7z -mx0 a ../grep-3.1.7z *
