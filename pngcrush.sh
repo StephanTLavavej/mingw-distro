fbe3e6bcb952c8b48af0ebf4c0e13c08ffb8aea4
Stephan T. Lavavej <stl@nuwen.net>
2017-08-06 16:06:19 -0700

    pngcrush 1.8.12.

--- pngcrush.sh
+++ pngcrush.sh
@@ -2,15 +2,15 @@
 
 source ./0_append_distro_path.sh
 
-extract_file pngcrush-1.8.11-nolib.tar
+extract_file pngcrush-1.8.12-nolib.tar
 
 cd /c/temp/gcc
-mv pngcrush-1.8.11-nolib src
+mv pngcrush-1.8.12-nolib src
 mkdir -p dest/bin
 
 gcc -s -O3 src/pngcrush.c -o dest/bin/pngcrush.exe -lpng -lz || fail_with pngcrush 1 - EPIC FAIL
 rm -rf src
-mv dest pngcrush-1.8.11
-cd pngcrush-1.8.11
+mv dest pngcrush-1.8.12
+cd pngcrush-1.8.12
 
-7z -mx0 a ../pngcrush-1.8.11.7z *
+7z -mx0 a ../pngcrush-1.8.12.7z *
