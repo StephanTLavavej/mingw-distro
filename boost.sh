0e57dd0872fbfe78bdcb5196408bf593327ca9b6
Stephan T. Lavavej <stl@nuwen.net>
2017-09-30 16:34:34 -0700

    Boost 1.65.1 fixes Boost.Stacktrace.

--- boost.sh
+++ boost.sh
@@ -2,17 +2,15 @@
 
 source ./0_append_distro_path.sh
 
-untar_file boost_1_65_0.tar
+untar_file boost_1_65_1.tar
 
 cd /c/temp/gcc
-mv boost_1_65_0 src
+mv boost_1_65_1 src
 mkdir -p dest/include
 cd src
 ./bootstrap.sh || fail_with boost 1 - EPIC FAIL
 
-# --without-stacktrace : A fix has been checked in for 1.66.0. (8/21/2017)
-./b2 $X_B2_JOBS --without-stacktrace \
-variant=release link=static runtime-link=static threading=multi --stagedir=/c/temp/gcc/dest stage \
+./b2 $X_B2_JOBS variant=release link=static runtime-link=static threading=multi --stagedir=/c/temp/gcc/dest stage \
 -sNO_BZIP2 -sBZIP2_BINARY=bz2 -sBZIP2_INCLUDE=$X_DISTRO_INC -sBZIP2_LIBPATH=$X_DISTRO_LIB \
 -sNO_ZLIB -sZLIB_BINARY=z -sZLIB_INCLUDE=$X_DISTRO_INC -sZLIB_LIBPATH=$X_DISTRO_LIB || fail_with boost 2 - EPIC FAIL
 
@@ -20,10 +18,10 @@ cd /c/temp/gcc/dest/lib
 for i in *.a; do mv $i ${i%-mgw*.a}.a; done
 cd /c/temp/gcc
 mv src/boost dest/include
-mv dest boost-1.65.0
+mv dest boost-1.65.1
 
-cd boost-1.65.0
-7z -mx0 a ../boost-1.65.0.7z * || fail_with boost-1.65.0.7z - EPIC FAIL
+cd boost-1.65.1
+7z -mx0 a ../boost-1.65.1.7z * || fail_with boost-1.65.1.7z - EPIC FAIL
 
 cd /c/temp/gcc
 rm -rf src
