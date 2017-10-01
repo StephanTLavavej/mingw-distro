60f761fe5cdc6c6fcc6d33785f0c25680a0a88a6
Stephan T. Lavavej <stl@nuwen.net>
2017-10-01 15:32:23 -0700

    coreutils 8.28.
    Regenerate coreutils.patch.

--- coreutils.sh
+++ coreutils.sh
@@ -2,12 +2,12 @@
 
 source ./0_append_distro_path.sh
 
-untar_file coreutils-8.27.tar
+untar_file coreutils-8.28.tar
 
-patch -d /c/temp/gcc/coreutils-8.27 -p1 < coreutils.patch
+patch -d /c/temp/gcc/coreutils-8.28 -p1 < coreutils.patch
 
 cd /c/temp/gcc
-mv coreutils-8.27 src
+mv coreutils-8.28 src
 mkdir -p build dest/bin
 
 # Missing <sys/wait.h>.
@@ -28,7 +28,7 @@ cd src
 mv sort.exe uniq.exe wc.exe ../../dest/bin || fail_with coreutils 2 - EPIC FAIL
 cd /c/temp/gcc
 rm -rf build src
-mv dest coreutils-8.27
-cd coreutils-8.27
+mv dest coreutils-8.28
+cd coreutils-8.28
 
-7z -mx0 a ../coreutils-8.27.7z *
+7z -mx0 a ../coreutils-8.28.7z *
