459174760af0391ab4e4a165f4a13617311b8156
Stephan T. Lavavej <stl@nuwen.net>
2017-10-01 13:36:30 -0700

    binutils 2.29.1.

--- binutils.sh
+++ binutils.sh
@@ -2,10 +2,10 @@
 
 source ./0_append_distro_path.sh
 
-untar_file binutils-2.29.tar
+untar_file binutils-2.29.1.tar
 
 cd /c/temp/gcc
-mv binutils-2.29 src
+mv binutils-2.29.1 src
 mkdir build dest
 cd build
 
@@ -17,8 +17,8 @@ make $X_MAKE_JOBS all "CFLAGS=-O3" "LDFLAGS=-s" || fail_with binutils 2 - EPIC F
 make install || fail_with binutils 3 - EPIC FAIL
 cd /c/temp/gcc
 rm -rf build src
-mv dest binutils-2.29
-cd binutils-2.29
+mv dest binutils-2.29.1
+cd binutils-2.29.1
 rm -rf lib/*.la share
 
-7z -mx0 a ../binutils-2.29.7z *
+7z -mx0 a ../binutils-2.29.1.7z *
