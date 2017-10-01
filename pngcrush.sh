0cec613f85471ca18d0f3643178a509450496ba6
Stephan T. Lavavej <stl@nuwen.net>
2017-10-01 16:15:37 -0700

    pngcrush 1.8.13.

--- pngcrush.sh
+++ pngcrush.sh
@@ -2,15 +2,15 @@
 
 source ./0_append_distro_path.sh
 
-untar_file pngcrush-1.8.12-nolib.tar
+untar_file pngcrush-1.8.13-nolib.tar
 
 cd /c/temp/gcc
-mv pngcrush-1.8.12-nolib src
+mv pngcrush-1.8.13-nolib src
 mkdir -p dest/bin
 
 gcc -s -O3 src/pngcrush.c -o dest/bin/pngcrush.exe -lpng -lz || fail_with pngcrush 1 - EPIC FAIL
 rm -rf src
-mv dest pngcrush-1.8.12
-cd pngcrush-1.8.12
+mv dest pngcrush-1.8.13
+cd pngcrush-1.8.13
 
-7z -mx0 a ../pngcrush-1.8.12.7z *
+7z -mx0 a ../pngcrush-1.8.13.7z *
