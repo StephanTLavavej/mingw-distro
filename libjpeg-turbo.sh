4fd77f65e194d9ef4062777a7308be890856b4d0
Stephan T. Lavavej <stl@nuwen.net>
2017-10-01 18:34:42 -0700

    Use `make install` in libjpeg-turbo.sh.
    Fixes #43.

--- libjpeg-turbo.sh
+++ libjpeg-turbo.sh
@@ -6,23 +6,18 @@ untar_file libjpeg-turbo-1.5.2.tar
 
 cd /c/temp/gcc
 mv libjpeg-turbo-1.5.2 src
-mkdir -p build dest/bin dest/include dest/lib
+mkdir build dest
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared \
 --prefix=/c/temp/gcc/dest "CFLAGS=-s -O3 -DTWO_FILE_COMMANDLINE" || fail_with libjpeg-turbo 1 - EPIC FAIL
 
-# make install must NOT be used, as it will contaminate the Windows system directory.
 make $X_MAKE_JOBS || fail_with libjpeg-turbo 2 - EPIC FAIL
+make install || fail_with libjpeg-turbo 3 - EPIC FAIL
 cd /c/temp/gcc
-mv build/jpegtran.exe dest/bin/jpegtran.exe || fail_with libjpeg-turbo 3 - EPIC FAIL
-
-mv build/jconfig.h src/jerror.h src/jmorecfg.h src/jpeglib.h src/turbojpeg.h dest/include \
-|| fail_with libjpeg-turbo 4 - EPIC FAIL
-
-mv build/.libs/libjpeg.a build/.libs/libturbojpeg.a dest/lib || fail_with libjpeg-turbo 5 - EPIC FAIL
 rm -rf build src
 mv dest libjpeg-turbo-1.5.2
 cd libjpeg-turbo-1.5.2
+rm -rf bin/cjpeg.exe bin/djpeg.exe bin/rdjpgcom.exe bin/tjbench.exe bin/wrjpgcom.exe lib/*.la lib/pkgconfig share
 
 7z -mx0 a ../libjpeg-turbo-1.5.2.7z *
