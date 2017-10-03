e121400ca7aeff1218e1c6544f05b9ba7b30f1e9
Stephan T. Lavavej <stl@nuwen.net>
2017-10-03 15:45:19 -0700

    Add 1_build_everything.sh.

---
+++ 1_build_everything.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+source ./0_append_distro_path.sh
+
+./binutils.sh
+./boost.sh
+./bzip2.sh
+./coreutils.sh
+./freetype.sh
+./gdb.sh
+./glbinding.sh
+./glm.sh
+./grep.sh
+./lame.sh
+./libjpeg-turbo.sh
+./libpng.sh
+./make.sh
+./mingw-w64+gcc.sh
+./pcre.sh
+./pcre2.sh
+./pngcheck.sh
+./pngcrush.sh
+./sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
+./sed.sh
+./zlib.sh
