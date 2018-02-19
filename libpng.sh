c8a3dad95e50370b8a268a890a58f5e67ea0d60f
Stephan T. Lavavej <stl@nuwen.net>
2018-02-18 19:57:08 -0800

    libpng 1.6.34.
    Issue #50: We need the linemarker workaround again.
    It was added in 22e4052 for libpng 1.6.17
    and removed in d22688b for libpng 1.6.18.

--- libpng.sh
+++ libpng.sh
@@ -2,22 +2,24 @@
 
 source ./0_append_distro_path.sh
 
-untar_file libpng-1.6.32.tar
+untar_file libpng-1.6.34.tar
 
 cd /c/temp/gcc
-mv libpng-1.6.32 src
+mv libpng-1.6.34 src
 mkdir build dest
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
 --prefix=/c/temp/gcc/dest --disable-shared
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
+# Adding -P avoids problems with linemarkers. Root cause unknown.
+make $X_MAKE_JOBS all "CFLAGS=-s -O3" "DFNCPP=gcc -E -P"
+
 make install
 cd /c/temp/gcc
 rm -rf build src
-mv dest libpng-1.6.32
-cd libpng-1.6.32
+mv dest libpng-1.6.34
+cd libpng-1.6.34
 rm -rf bin include/libpng16 lib/pkgconfig lib/*.la lib/libpng16.a share
 
-7z -mx0 a ../libpng-1.6.32.7z *
+7z -mx0 a ../libpng-1.6.34.7z *
