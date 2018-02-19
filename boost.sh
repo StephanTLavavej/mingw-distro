8823be01f6759bb29433a5cce4f1995d656fe22e
Stephan T. Lavavej <stl@nuwen.net>
2018-02-18 16:53:52 -0800

    Boost 1.66.0.
    Boost now builds x32 and x64 by default.
    Adding address-model=64 prevents this.

--- boost.sh
+++ boost.sh
@@ -2,15 +2,16 @@
 
 source ./0_append_distro_path.sh
 
-untar_file boost_1_65_1.tar
+untar_file boost_1_66_0.tar
 
 cd /c/temp/gcc
-mv boost_1_65_1 src
+mv boost_1_66_0 src
 mkdir -p dest/include
 cd src
 ./bootstrap.sh
 
 ./b2 $X_B2_JOBS variant=release link=static runtime-link=static threading=multi --stagedir=/c/temp/gcc/dest stage \
+address-model=64 \
 -sNO_BZIP2 -sBZIP2_BINARY=bz2 -sBZIP2_INCLUDE=$X_DISTRO_INC -sBZIP2_LIBPATH=$X_DISTRO_LIB \
 -sNO_ZLIB -sZLIB_BINARY=z -sZLIB_INCLUDE=$X_DISTRO_INC -sZLIB_LIBPATH=$X_DISTRO_LIB
 
@@ -20,7 +21,7 @@ cd /c/temp/gcc
 mv src/boost dest/include
 rm -rf src
 
-mv dest boost-1.65.1
-cd boost-1.65.1
+mv dest boost-1.66.0
+cd boost-1.66.0
 
-7z -mx0 a ../boost-1.65.1.7z *
+7z -mx0 a ../boost-1.66.0.7z *
