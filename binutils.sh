c1c36a7aa4a2ba76c88c60c30036e998831f7067
Stephan T. Lavavej <stl@nuwen.net>
2018-06-24 20:34:17 -0700

    binutils 2.30.
    binutils-bug-22762.patch: #51 has been fixed upstream.

--- binutils.sh
+++ binutils.sh
@@ -2,10 +2,14 @@
 
 source ./0_append_distro_path.sh
 
-untar_file binutils-2.29.1.tar
+untar_file binutils-2.30.tar
+
+# https://github.com/StephanTLavavej/mingw-distro/issues/51
+# Fixed upstream: https://sourceware.org/bugzilla/show_bug.cgi?id=22762
+patch -d /c/temp/gcc/binutils-2.30 -p1 < binutils-bug-22762.patch
 
 cd /c/temp/gcc
-mv binutils-2.29.1 src
+mv binutils-2.30 src
 mkdir build dest
 cd build
 
@@ -16,8 +20,8 @@ make $X_MAKE_JOBS all "CFLAGS=-O3" "LDFLAGS=-s"
 make install
 cd /c/temp/gcc
 rm -rf build src
-mv dest binutils-2.29.1
-cd binutils-2.29.1
+mv dest binutils-2.30
+cd binutils-2.30
 rm -rf lib/*.la share
 
-7z -mx0 a ../binutils-2.29.1.7z *
+7z -mx0 a ../binutils-2.30.7z *
