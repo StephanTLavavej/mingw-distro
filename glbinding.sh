6bfa1ce31b4a4a6037d938a6b7fa0ead3d3928bc
Stephan T. Lavavej <stl@nuwen.net>
2018-12-23 14:19:01 -0800

    glbinding 3.0.2.
    OPENGL_gl_LIBRARY and OPENGL_INCLUDE_DIR are now unused.
    
    Disable OPTION_BUILD_EXAMPLES to avoid a CMake warning.

--- glbinding.sh
+++ glbinding.sh
@@ -2,10 +2,10 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glbinding-2.1.4.tar
+untar_file glbinding-3.0.2.tar
 
 cd /c/temp/gcc
-mv glbinding-2.1.4 src
+mv glbinding-3.0.2 src
 mkdir build dest
 cd build
 
@@ -13,8 +13,7 @@ cmake \
 "-DBUILD_SHARED_LIBS=OFF" \
 "-DCMAKE_CXX_FLAGS=-DBOOST_THREAD_VERSION=4 -s -O3 -DSYSTEM_WINDOWS" \
 "-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
-"-DOPENGL_gl_LIBRARY=/c/mingw/x86_64-w64-mingw32/lib/libopengl32.a" \
-"-DOPENGL_INCLUDE_DIR=/c/mingw/x86_64-w64-mingw32/include/gl" \
+"-DOPTION_BUILD_EXAMPLES=OFF" \
 "-DOPTION_BUILD_TESTS=OFF" \
 "-DOPTION_BUILD_TOOLS=OFF" \
 "-DOPTION_BUILD_WITH_BOOST_THREAD=ON" \
@@ -24,9 +23,9 @@ make $X_MAKE_JOBS
 make $X_MAKE_JOBS install
 cd /c/temp/gcc
 rm -rf build src
-mv dest glbinding-2.1.4
-cd glbinding-2.1.4
+mv dest glbinding-3.0.2
+cd glbinding-3.0.2
 rm -rf cmake AUTHORS glbinding-config.cmake README.md VERSION
 mv LICENSE include/glbinding
 
-7z -mx0 a ../glbinding-2.1.4.7z *
+7z -mx0 a ../glbinding-3.0.2.7z *
