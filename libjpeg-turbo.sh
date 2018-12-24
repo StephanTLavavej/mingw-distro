cff8c0052016d89fc4b06155226f70974ca6ef74
Stephan T. Lavavej <stl@nuwen.net>
2018-12-23 17:04:27 -0800

    libjpeg-turbo 2.0.1.
    libjpeg-turbo now uses CMake.
    
    Setting CMAKE_ASM_NASM_FLAGS and CMAKE_ASM_NASM_OBJECT_FORMAT
    is necessary because their defaults are incorrect.
    
    The build no longer generates `lib/*.la`.

--- libjpeg-turbo.sh
+++ libjpeg-turbo.sh
@@ -2,22 +2,27 @@
 
 source ./0_append_distro_path.sh
 
-untar_file libjpeg-turbo-1.5.3.tar
+untar_file libjpeg-turbo-2.0.1.tar
 
 cd /c/temp/gcc
-mv libjpeg-turbo-1.5.3 src
+mv libjpeg-turbo-2.0.1 src
 mkdir build dest
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared \
---prefix=/c/temp/gcc/dest "CFLAGS=-s -O3 -DTWO_FILE_COMMANDLINE"
+cmake \
+"-DCMAKE_ASM_NASM_FLAGS=-DWIN64" \
+"-DCMAKE_ASM_NASM_OBJECT_FORMAT=win64" \
+"-DCMAKE_C_FLAGS=-s -O3 -DTWO_FILE_COMMANDLINE" \
+"-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
+"-DENABLE_SHARED=OFF" \
+-G "Unix Makefiles" /c/temp/gcc/src
 
 make $X_MAKE_JOBS
 make $X_MAKE_JOBS install
 cd /c/temp/gcc
 rm -rf build src
-mv dest libjpeg-turbo-1.5.3
-cd libjpeg-turbo-1.5.3
-rm -rf bin/cjpeg.exe bin/djpeg.exe bin/rdjpgcom.exe bin/tjbench.exe bin/wrjpgcom.exe lib/*.la lib/pkgconfig share
+mv dest libjpeg-turbo-2.0.1
+cd libjpeg-turbo-2.0.1
+rm -rf bin/cjpeg.exe bin/djpeg.exe bin/rdjpgcom.exe bin/tjbench.exe bin/wrjpgcom.exe lib/pkgconfig share
 
-7z -mx0 a ../libjpeg-turbo-1.5.3.7z *
+7z -mx0 a ../libjpeg-turbo-2.0.1.7z *
