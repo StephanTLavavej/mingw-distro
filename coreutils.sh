75b63ae387b6c7a74b8e70893115f35f74a06f16
Stephan T. Lavavej <stl@nuwen.net>
2019-11-24 14:54:15 -0800

    coreutils 8.31.
    coreutils.patch: Regenerate.
    
    coreutils.sh: Skip building src/lib/backupfile.c.

--- coreutils.sh
+++ coreutils.sh
@@ -2,12 +2,12 @@
 
 source ./0_append_distro_path.sh
 
-untar_file coreutils-8.30.tar
+untar_file coreutils-8.31.tar
 
-patch -d /c/temp/gcc/coreutils-8.30 -p1 < coreutils.patch
+patch -d /c/temp/gcc/coreutils-8.31 -p1 < coreutils.patch
 
 cd /c/temp/gcc
-mv coreutils-8.30 src
+mv coreutils-8.31 src
 mkdir -p build dest/bin
 
 # Missing <sys/wait.h>.
@@ -16,6 +16,9 @@ echo "/* ignore */" > src/lib/savewd.c
 # Missing <pwd.h> and <grp.h>.
 echo "/* ignore */" > src/lib/idcache.c
 echo "/* ignore */" > src/lib/userspec.c
+
+# Missing fpathconf().
+echo "/* ignore */" > src/lib/backupfile.c
 
 cd build
 echo "ac_cv_header_pthread_h=no" > config.site
@@ -30,7 +33,7 @@ cd src
 mv sort.exe uniq.exe wc.exe ../../dest/bin
 cd /c/temp/gcc
 rm -rf build src
-mv dest coreutils-8.30
-cd coreutils-8.30
+mv dest coreutils-8.31
+cd coreutils-8.31
 
-7z -mx0 a ../coreutils-8.30.7z *
+7z -mx0 a ../coreutils-8.31.7z *
