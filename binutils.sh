d6977fab930be9483f0bafcc099699281c0e3d55
Stephan T. Lavavej <stl@nuwen.net>
2021-09-06 14:32:17 -0700

    binutils 2.37.
    binutils-fix-uint.patch: This MinGW build break has been fixed upstream.

--- binutils.sh
+++ binutils.sh
@@ -2,10 +2,13 @@
 
 source ./0_append_distro_path.sh
 
-untar_file binutils-2.33.1.tar
+untar_file binutils-2.37.tar
+
+# https://sourceware.org/git/?p=binutils-gdb.git;a=commit;h=999566402e3d7c69032bbf47e28b44fc0926fe62
+patch -d /c/temp/gcc/binutils-2.37 -p1 < binutils-fix-uint.patch
 
 cd /c/temp/gcc
-mv binutils-2.33.1 src
+mv binutils-2.37 src
 mkdir build dest
 cd build
 
@@ -16,8 +19,8 @@ make $X_MAKE_JOBS all "CFLAGS=-O3" "LDFLAGS=-s"
 make $X_MAKE_JOBS install
 cd /c/temp/gcc
 rm -rf build src
-mv dest binutils-2.33.1
-cd binutils-2.33.1
+mv dest binutils-2.37
+cd binutils-2.37
 rm -rf lib/*.la share
 
-7z -mx0 a ../binutils-2.33.1.7z *
+7z -mx0 a ../binutils-2.37.7z *
