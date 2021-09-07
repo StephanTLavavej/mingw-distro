51f65948125981cf1057214689bb60b005464d28
Stephan T. Lavavej <stl@nuwen.net>
2021-09-06 19:33:39 -0700

    zstd 1.5.0.
    Also use Ninja.
    
    Delete `lib/cmake` which is now being produced.

--- zstd.sh
+++ zstd.sh
@@ -2,10 +2,10 @@
 
 source ./0_append_distro_path.sh
 
-untar_file zstd-1.4.4.tar
+untar_file zstd-1.5.0.tar
 
 cd /c/temp/gcc
-mv zstd-1.4.4 src
+mv zstd-1.5.0 src
 mkdir build dest
 cd build
 
@@ -13,15 +13,15 @@ cmake \
 "-DCMAKE_C_FLAGS=-s -O3" \
 "-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
 "-DZSTD_BUILD_SHARED=OFF" \
--G "Unix Makefiles" /c/temp/gcc/src/build/cmake
+-G Ninja /c/temp/gcc/src/build/cmake
 
-make $X_MAKE_JOBS
-make $X_MAKE_JOBS install
+ninja
+ninja install
 cd /c/temp/gcc
 rm -rf build src
-mv dest zstd-1.4.4
-cd zstd-1.4.4
-rm -rf bin/zstdgrep bin/zstdless lib/pkgconfig share
+mv dest zstd-1.5.0
+cd zstd-1.5.0
+rm -rf bin/zstdgrep bin/zstdless lib/cmake lib/pkgconfig share
 for i in bin/unzstd bin/zstdcat bin/zstdmt; do mv $i $i.exe; done
 
-7z -mx0 a ../zstd-1.4.4.7z *
+7z -mx0 a ../zstd-1.5.0.7z *
