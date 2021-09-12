7ae9519201d730bf0677f685ebeea2f58aec2258
Stephan T. Lavavej <stl@nuwen.net>
2021-09-11 20:04:12 -0700

    pcre.sh: Use CMake/Ninja.

--- pcre.sh
+++ pcre.sh
@@ -9,17 +9,26 @@ mv pcre-8.45 src
 mkdir build dest
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=/c/temp/gcc/dest --disable-shared --enable-utf --enable-unicode-properties --enable-pcre16 --enable-pcre32 \
---enable-jit --enable-newline-is-anycrlf "CFLAGS=-s -O3" "CXXFLAGS=-s -O3"
+cmake \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_CXX_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
+"-DPCRE_BUILD_PCRE16=ON" \
+"-DPCRE_BUILD_PCRE32=ON" \
+"-DPCRE_BUILD_TESTS=OFF" \
+"-DPCRE_NEWLINE=ANYCRLF" \
+"-DPCRE_SUPPORT_JIT=ON" \
+"-DPCRE_SUPPORT_UNICODE_PROPERTIES=ON" \
+"-DPCRE_SUPPORT_UTF=ON" \
+-G Ninja /c/temp/gcc/src
 
-make $X_MAKE_JOBS all
-make $X_MAKE_JOBS install
+ninja
+ninja install
 cd /c/temp/gcc
 rm -rf build src
 mv dest pcre-8.45
 cd pcre-8.45
-rm -rf bin/pcre-config lib/pkgconfig lib/*.la share
+rm -rf bin/pcre-config lib/pkgconfig man share
 cp include/pcreposix.h include/regex.h
 
 7z -mx0 a ../pcre-8.45.7z *
