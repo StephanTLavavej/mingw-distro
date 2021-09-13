edbe46da18ed843cf71000367a1f012327e4a6d1
Stephan T. Lavavej <stl@nuwen.net>
2021-09-12 17:17:12 -0700

    FreeType: Use CMake/Ninja.
    freetype-fix-cmake-mingw.patch:
    We need to fix CMakeLists.txt to properly handle MinGW.

--- freetype.sh
+++ freetype.sh
@@ -4,20 +4,24 @@ source ./0_append_distro_path.sh
 
 untar_file freetype-2.11.0.tar
 
+patch -d /c/temp/gcc/freetype-2.11.0 -p1 < freetype-fix-cmake-mingw.patch
+
 cd /c/temp/gcc
 mv freetype-2.11.0 src
 mkdir build dest
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-shared \
---prefix=/c/temp/gcc/dest "CFLAGS=-s -O3"
+cmake \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
+-G Ninja /c/temp/gcc/src
 
-make $X_MAKE_JOBS all
-make $X_MAKE_JOBS install
+ninja
+ninja install
 cd /c/temp/gcc
 rm -rf build src
 mv dest freetype-2.11.0
 cd freetype-2.11.0
-rm -rf lib/pkgconfig lib/*.la share
+rm -rf lib/cmake lib/pkgconfig
 
 7z -mx0 a ../freetype-2.11.0.7z *
