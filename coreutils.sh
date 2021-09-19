64dd5852915e8422837f8938ca8ca2f5d48257d2
Stephan T. Lavavej <stl@nuwen.net>
2021-09-19 14:32:45 -0700

    coreutils 8.32.
    coreutils.patch: Regenerate. One uniq.c issue was fixed upstream, improving #4.

--- coreutils.sh
+++ coreutils.sh
@@ -2,12 +2,12 @@
 
 source ./0_append_distro_path.sh
 
-untar_file coreutils-8.31.tar
+untar_file coreutils-8.32.tar
 
-patch -d /c/temp/gcc/coreutils-8.31 -p1 < coreutils.patch
+patch -d /c/temp/gcc/coreutils-8.32 -p1 < coreutils.patch
 
 cd /c/temp/gcc
-mv coreutils-8.31 src
+mv coreutils-8.32 src
 mkdir -p build dest/bin
 
 # Missing <sys/wait.h>.
@@ -34,7 +34,7 @@ cd src
 mv sort.exe uniq.exe wc.exe ../../dest/bin
 cd /c/temp/gcc
 rm -rf build src
-mv dest coreutils-8.31
-cd coreutils-8.31
+mv dest coreutils-8.32
+cd coreutils-8.32
 
-7z -mx0 a ../coreutils-8.31.7z *
+7z -mx0 a ../coreutils-8.32.7z *
