2bf83d1721806383748e9b536356656464b07ce1
Stephan T. Lavavej <stl@nuwen.net>
2021-09-19 15:11:45 -0700

    libpng.sh: Use CMake/Ninja.

--- libpng.sh
+++ libpng.sh
@@ -9,15 +9,19 @@ mv libpng-1.6.37 src
 mkdir build dest
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=/c/temp/gcc/dest --disable-shared
+cmake \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=/c/temp/gcc/dest" \
+"-DM_LIBRARY=" \
+"-DPNG_SHARED=OFF" \
+-G Ninja /c/temp/gcc/src
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
-make $X_MAKE_JOBS install
+ninja
+ninja install
 cd /c/temp/gcc
 rm -rf build src
 mv dest libpng-1.6.37
 cd libpng-1.6.37
-rm -rf bin include/libpng16 lib/pkgconfig lib/*.la lib/libpng16.a share
+rm -rf bin include/libpng16 lib/libpng lib/pkgconfig lib/libpng16.a share
 
 7z -mx0 a ../libpng-1.6.37.7z *
