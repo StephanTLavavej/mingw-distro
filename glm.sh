6b28b24d3e6f68c86c189af2be19f29df62b904f
Stephan T. Lavavej <stl@nuwen.net>
2021-09-19 14:42:56 -0700

    GLM 0.9.9.8.

--- glm.sh
+++ glm.sh
@@ -2,14 +2,14 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glm-0.9.9.6.tar
+untar_file glm-0.9.9.8.tar
 
 cd /c/temp/gcc
-mv glm-0.9.9.6 src
+mv glm-0.9.9.8 src
 mkdir -p dest/include
 mv src/glm dest/include
 rm -rf src
-mv dest glm-0.9.9.6
-cd glm-0.9.9.6
+mv dest glm-0.9.9.8
+cd glm-0.9.9.8
 
-7z -mx0 a ../glm-0.9.9.6.7z *
+7z -mx0 a ../glm-0.9.9.8.7z *
