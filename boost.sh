32ff7aa81c5509a3d24a53f6df8a30167c162d3d
Stephan T. Lavavej <stl@nuwen.net>
2024-01-05 16:37:21 -0800

    Boost 1.84.0.
    boost-fix-raw_maxline.patch works around #95.

--- boost.sh
+++ boost.sh
@@ -2,10 +2,13 @@
 
 source ./0_append_distro_path.sh
 
-untar_file boost_1_77_0.tar
+untar_file boost_1_84_0.tar
+
+# https://github.com/StephanTLavavej/mingw-distro/issues/95
+patch -d $X_WORK_DIR/boost_1_84_0 -p1 < boost-fix-raw_maxline.patch
 
 cd $X_WORK_DIR
-mv boost_1_77_0 src
+mv boost_1_84_0 src
 mkdir -p dest/include
 cd src
 
@@ -20,8 +23,8 @@ cd $X_WORK_DIR
 mv src/boost dest/include
 rm -rf src
 
-mv dest boost-1.77.0
-cd boost-1.77.0
+mv dest boost-1.84.0
+cd boost-1.84.0
 rm -rf lib/cmake
 
-7z -mx0 a ../boost-1.77.0.7z *
+7z -mx0 a ../boost-1.84.0.7z *
