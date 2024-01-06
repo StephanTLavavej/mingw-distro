31dfbfab4429d9ece7cd86dde986252532fa4ba9
Stephan T. Lavavej <stl@nuwen.net>
2024-01-05 20:44:51 -0800

    glbinding 3.3.0.
    glbinding-pr-298.patch has been upstreamed.
    
    We now need to set BOOST_ROOT.

--- glbinding.sh
+++ glbinding.sh
@@ -2,17 +2,15 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glbinding-3.1.0.tar
-
-# https://github.com/cginternals/glbinding/pull/298 fixes https://github.com/StephanTLavavej/mingw-distro/issues/65
-patch -d $X_WORK_DIR/glbinding-3.1.0 -p1 < glbinding-pr-298.patch
+untar_file glbinding-3.3.0.tar
 
 cd $X_WORK_DIR
-mv glbinding-3.1.0 src
+mv glbinding-3.3.0 src
 mkdir build dest
 cd build
 
 cmake \
+"-DBOOST_ROOT=$X_DISTRO_ROOT" \
 "-DBUILD_SHARED_LIBS=OFF" \
 "-DCMAKE_BUILD_TYPE=Release" \
 "-DCMAKE_CXX_FLAGS=-DBOOST_THREAD_VERSION=4 -s -O3 -DSYSTEM_WINDOWS" \
@@ -27,9 +25,9 @@ ninja
 ninja install
 cd $X_WORK_DIR
 rm -rf build src
-mv dest glbinding-3.1.0
-cd glbinding-3.1.0
+mv dest glbinding-3.3.0
+cd glbinding-3.3.0
 rm -rf cmake AUTHORS glbinding-config.cmake README.md VERSION
 mv LICENSE include/glbinding
 
-7z -mx0 a ../glbinding-3.1.0.7z *
+7z -mx0 a ../glbinding-3.3.0.7z *
