6d2fca6da70b0ddb1a905552433e028a301ff0bb
Stephan T. Lavavej <stl@nuwen.net>
2024-01-07 12:50:38 -0800

    FreeType 2.13.2.
    freetype-fix-cmake-mingw.patch works around #97.

--- freetype.sh
+++ freetype.sh
@@ -2,12 +2,13 @@
 
 source ./0_append_distro_path.sh
 
-untar_file freetype-2.11.0.tar
+untar_file freetype-2.13.2.tar
 
-patch -d $X_WORK_DIR/freetype-2.11.0 -p1 < freetype-fix-cmake-mingw.patch
+# https://github.com/StephanTLavavej/mingw-distro/issues/97
+patch -d $X_WORK_DIR/freetype-2.13.2 -p1 < freetype-fix-cmake-mingw.patch
 
 cd $X_WORK_DIR
-mv freetype-2.11.0 src
+mv freetype-2.13.2 src
 mkdir build dest
 cd build
 
@@ -21,8 +22,8 @@ ninja
 ninja install
 cd $X_WORK_DIR
 rm -rf build src
-mv dest freetype-2.11.0
-cd freetype-2.11.0
+mv dest freetype-2.13.2
+cd freetype-2.13.2
 rm -rf lib/cmake lib/pkgconfig
 
-7z -mx0 a ../freetype-2.11.0.7z *
+7z -mx0 a ../freetype-2.13.2.7z *
