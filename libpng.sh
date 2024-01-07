634dd9bcd75ea7ee2ef1acea6fff6f0ebf2cfb74
Stephan T. Lavavej <stl@nuwen.net>
2024-01-07 14:23:25 -0800

    libpng 1.6.40.
    We now need to set ZLIB_ROOT.

--- libpng.sh
+++ libpng.sh
@@ -2,10 +2,10 @@
 
 source ./0_append_distro_path.sh
 
-untar_file libpng-1.6.37.tar
+untar_file libpng-1.6.40.tar
 
 cd $X_WORK_DIR
-mv libpng-1.6.37 src
+mv libpng-1.6.40 src
 mkdir build dest
 cd build
 
@@ -15,14 +15,15 @@ cmake \
 "-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
 "-DM_LIBRARY=" \
 "-DPNG_SHARED=OFF" \
+"-DZLIB_ROOT=$X_DISTRO_ROOT" \
 -G Ninja $X_WORK_DIR/src
 
 ninja
 ninja install
 cd $X_WORK_DIR
 rm -rf build src
-mv dest libpng-1.6.37
-cd libpng-1.6.37
+mv dest libpng-1.6.40
+cd libpng-1.6.40
 rm -rf bin include/libpng16 lib/libpng lib/pkgconfig lib/libpng16.a share
 
-7z -mx0 a ../libpng-1.6.37.7z *
+7z -mx0 a ../libpng-1.6.40.7z *
