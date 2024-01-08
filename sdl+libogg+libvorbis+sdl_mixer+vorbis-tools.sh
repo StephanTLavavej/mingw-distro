c5090c81864374bbf909a12f90084ef9bb4060c5
Stephan T. Lavavej <stl@nuwen.net>
2024-01-07 18:47:09 -0800

    SDL 2.28.5 and SDL_mixer 2.6.3.
    We need to work around #98.

--- sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
+++ sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
@@ -2,15 +2,15 @@
 
 source ./0_append_distro_path.sh
 
-untar_file SDL2-2.0.16.tar
+untar_file SDL2-2.28.5.tar
 untar_file libogg-1.3.5.tar
 untar_file libvorbis-1.3.7.tar
-untar_file SDL2_mixer-2.0.4.tar --exclude=SDL2_mixer-2.0.4/Xcode
+untar_file SDL2_mixer-2.6.3.tar --exclude=SDL2_mixer-2.6.3/Xcode
 untar_file vorbis-tools-1.4.2.tar
 
 cd $X_WORK_DIR
 
-mv SDL2-2.0.16 src
+mv SDL2-2.28.5 src
 mkdir build dest
 cd build
 
@@ -46,14 +46,15 @@ make $X_MAKE_JOBS install
 cd $X_WORK_DIR
 rm -rf build src
 
-mv SDL2_mixer-2.0.4 src
+mv SDL2_mixer-2.6.3 src
 mkdir build
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
 --prefix=$X_WORK_DIR/dest --disable-shared
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
+# The `-I` options work around bugs in the build system: https://github.com/StephanTLavavej/mingw-distro/issues/98
+make $X_MAKE_JOBS all "CFLAGS=-s -O3 -I$X_WORK_DIR/src/include -I$X_WORK_DIR/src/src -I$X_WORK_DIR/src/src/codecs"
 make $X_MAKE_JOBS install
 cd $X_WORK_DIR
 rm -rf build src
