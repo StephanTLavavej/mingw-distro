3793be9e924dc33a51d0ab237ff2e6bb08cbd06c
Stephan T. Lavavej <stl@nuwen.net>
2024-01-07 20:17:26 -0800

    grep 3.11.
    This now uses PCRE2, so we need to define `PCRE2_STATIC`.
    
    Fixes #89.

--- grep.sh
+++ grep.sh
@@ -2,24 +2,24 @@
 
 source ./0_append_distro_path.sh
 
-untar_file grep-3.7.tar
+untar_file grep-3.11.tar
 
 cd $X_WORK_DIR
 mkdir -p dest/bin
 
-mv grep-3.7 src
+mv grep-3.11 src
 mkdir build
 cd build
 
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=$X_WORK_DIR/dest --disable-nls "CFLAGS=-s -O3 -DPCRE_STATIC"
+--prefix=$X_WORK_DIR/dest --disable-nls "CFLAGS=-s -O3 -DPCRE2_STATIC"
 
 make $X_MAKE_JOBS
 mv src/grep.exe ../dest/bin
 cd $X_WORK_DIR
 rm -rf build src
 
-mv dest grep-3.7
-cd grep-3.7
+mv dest grep-3.11
+cd grep-3.11
 
-7z -mx0 a ../grep-3.7.7z *
+7z -mx0 a ../grep-3.11.7z *
