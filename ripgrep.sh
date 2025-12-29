cb73369ab318c5c7f842147eeba5aae75b277454
Stephan T. Lavavej <stl@nuwen.net>
2025-12-29 13:49:14 -0800

    ripgrep 15.1.0.
    We need to link statically via `RUSTFLAGS`, otherwise the
    resulting executable will fail to load libunwind.dll.

--- ripgrep.sh
+++ ripgrep.sh
@@ -3,20 +3,22 @@
 source ./0_append_distro_path.sh
 
 # Work around https://github.com/msys2/MSYS2-packages/issues/1216 by excluding the affected symlink.
-untar_file ripgrep-14.1.0.tar --exclude=ripgrep-14.1.0/HomebrewFormula
+untar_file ripgrep-15.1.0.tar --exclude=ripgrep-15.1.0/HomebrewFormula
 
 cd $X_WORK_DIR
-mv ripgrep-14.1.0 src
+mv ripgrep-15.1.0 src
 mkdir dest
 cd src
 
 export PATH=$PATH:/clang64/bin
+# Link to the CRT statically in order to avoid "The code execution cannot proceed because libunwind.dll was not found."
+export RUSTFLAGS="-Ctarget-feature=+crt-static"
 cargo install --profile release-lto --features 'pcre2' --path . --locked --root $X_WORK_DIR/dest
 
 cd $X_WORK_DIR
 rm -rf src
-mv dest ripgrep-14.1.0
-cd ripgrep-14.1.0
+mv dest ripgrep-15.1.0
+cd ripgrep-15.1.0
 rm -rf .crates.toml .crates2.json
 
-7z -mx0 a ../ripgrep-14.1.0.7z *
+7z -mx0 a ../ripgrep-15.1.0.7z *
