e6b6aaabc21cb431ba7731e50217222c09636b77
Stephan T. Lavavej <stl@nuwen.net>
2025-12-29 12:33:46 -0800

    pngcheck 4.0.1.
    Switch from manual gcc invocation to cmake/ninja.

--- pngcheck.sh
+++ pngcheck.sh
@@ -2,15 +2,29 @@
 
 source ./0_append_distro_path.sh
 
-untar_file pngcheck-3.0.3.tar
+untar_file pngcheck-4.0.1.tar
 
 cd $X_WORK_DIR
-mv pngcheck-3.0.3 src
-mkdir -p dest/bin
+mv pngcheck-4.0.1 src
+mkdir build dest
+cd build
 
-gcc -s -O3 src/pngcheck.c -o dest/bin/pngcheck.exe
-rm -rf src
-mv dest pngcheck-3.0.3
-cd pngcheck-3.0.3
+export PATH=$PATH:/mingw64/bin
+# Set ZLIB_ROOT to pick it up from the distro instead of /mingw64/bin.
+cmake \
+"-DCMAKE_BUILD_TYPE=Release" \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DZLIB_ROOT=$X_DISTRO_ROOT" \
+-G Ninja $X_WORK_DIR/src
 
-7z -mx0 a ../pngcheck-3.0.3.7z *
+ninja
+ninja install
+
+cd $X_WORK_DIR
+rm -rf build src
+mv dest pngcheck-4.0.1
+cd pngcheck-4.0.1
+rm -rf share
+
+7z -mx0 a ../pngcheck-4.0.1.7z *
