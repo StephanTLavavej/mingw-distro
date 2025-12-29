531d4cb1727aa3a4c56b38cbc6e9b8abafba3c51
Stephan T. Lavavej <stl@nuwen.net>
2025-12-29 13:18:57 -0800

    uutils/coreutils 0.5.0.
    We need to link statically via `RUSTFLAGS`, otherwise the
    resulting executable will fail to load libunwind.dll.

--- uutils-coreutils.sh
+++ uutils-coreutils.sh
@@ -3,20 +3,22 @@
 source ./0_append_distro_path.sh
 
 # https://github.com/uutils/coreutils
-untar_file coreutils-0.0.23.tar
+untar_file coreutils-0.5.0.tar
 
 cd $X_WORK_DIR
-mv coreutils-0.0.23 src
+mv coreutils-0.5.0 src
 mkdir dest
 cd src
 
 export PATH=$PATH:/clang64/bin
+# Link to the CRT statically in order to avoid "The code execution cannot proceed because libunwind.dll was not found."
+export RUSTFLAGS="-Ctarget-feature=+crt-static"
 cargo install --path . --locked --root $X_WORK_DIR/dest
 
 cd $X_WORK_DIR
 rm -rf src
-mv dest uutils-coreutils-0.0.23
-cd uutils-coreutils-0.0.23
+mv dest uutils-coreutils-0.5.0
+cd uutils-coreutils-0.5.0
 rm -rf .crates.toml .crates2.json
 
-7z -mx0 a ../uutils-coreutils-0.0.23.7z *
+7z -mx0 a ../uutils-coreutils-0.5.0.7z *
