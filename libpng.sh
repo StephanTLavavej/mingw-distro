88539d566371b1dfa4106caf596e95dc86e7afa7
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:30:52 -0800

    libpng 1.6.53.
    We no longer need to set `M_LIBRARY`.
    
    Clean up a new `lib/cmake` directory.

--- libpng.sh
+++ libpng.sh
@@ -2,28 +2,30 @@
 
 source ./0_append_distro_path.sh
 
-untar_file libpng-1.6.40.tar
+untar_file libpng-1.6.53.tar
 
 cd $X_WORK_DIR
-mv libpng-1.6.40 src
+mv libpng-1.6.53 src
 mkdir build dest
 cd build
 
+export PATH=$PATH:/mingw64/bin
+# Set ZLIB_ROOT to pick it up from the distro instead of /mingw64/bin.
 cmake \
 "-DCMAKE_BUILD_TYPE=Release" \
 "-DCMAKE_C_FLAGS=-s -O3" \
 "-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
-"-DM_LIBRARY=" \
 "-DPNG_SHARED=OFF" \
 "-DZLIB_ROOT=$X_DISTRO_ROOT" \
 -G Ninja $X_WORK_DIR/src
 
 ninja
 ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
-mv dest libpng-1.6.40
-cd libpng-1.6.40
-rm -rf bin include/libpng16 lib/libpng lib/pkgconfig lib/libpng16.a share
+mv dest libpng-1.6.53
+cd libpng-1.6.53
+rm -rf bin include/libpng16 lib/cmake lib/libpng lib/pkgconfig lib/libpng16.a share
 
-7z -mx0 a ../libpng-1.6.40.7z *
+7z -mx0 a ../libpng-1.6.53.7z *
