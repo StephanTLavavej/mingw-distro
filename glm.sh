bcf79983a8fb6b12a358f546eb2291e4ab280da2
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:16:31 -0800

    GLM 1.0.3.

--- glm.sh
+++ glm.sh
@@ -2,14 +2,14 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glm-0.9.9.8.tar
+untar_file glm-1.0.3.tar
 
 cd $X_WORK_DIR
-mv glm-0.9.9.8 src
+mv glm-1.0.3 src
 mkdir -p dest/include
 mv src/glm dest/include
 rm -rf src
-mv dest glm-0.9.9.8
-cd glm-0.9.9.8
+mv dest glm-1.0.3
+cd glm-1.0.3
 
-7z -mx0 a ../glm-0.9.9.8.7z *
+7z -mx0 a ../glm-1.0.3.7z *
