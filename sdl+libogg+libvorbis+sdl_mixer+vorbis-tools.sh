f2dc8df35d264e84ebf6cb8b029da2dbbe06ebc7
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 09:36:20 -0800

    SDL 2.32.10, libogg 1.3.6, SDL_mixer 2.8.1, vorbis-tools 1.4.3.
    We no longer need to `--exclude` SDL_mixer's `Xcode` directory to avoid problems with symlinks.
    
    Patch vorbis-tools to work around #118.
    
    Switch from configure/make to cmake/ninja (except for vorbis-tools). Fixes #98. Fixes #101.
    
    We no longer need to cleanup `lib/*.la`.
    
    Configure libvorbis with `CMAKE_POLICY_VERSION_MINIMUM` to work around #113.
    
    Created tracking issue #114 "vorbis-tools 1.4.3 doesn't support CMake".
    
    Compile vorbis-tools with `-std=gnu17` to work around #116.

--- sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
+++ sdl+libogg+libvorbis+sdl_mixer+vorbis-tools.sh
@@ -2,35 +2,49 @@
 
 source ./0_append_distro_path.sh
 
-untar_file SDL2-2.28.5.tar
-untar_file libogg-1.3.5.tar
+untar_file SDL2-2.32.10.tar
+untar_file libogg-1.3.6.tar
 untar_file libvorbis-1.3.7.tar
-untar_file SDL2_mixer-2.6.3.tar --exclude=SDL2_mixer-2.6.3/Xcode
-untar_file vorbis-tools-1.4.2.tar
+untar_file SDL2_mixer-2.8.1.tar
+untar_file vorbis-tools-1.4.3.tar
+
+# https://github.com/StephanTLavavej/mingw-distro/issues/118
+patch -d $X_WORK_DIR/vorbis-tools-1.4.3 -p1 < vorbis-tools-fix-convert_free_charset.patch
 
 cd $X_WORK_DIR
+export PATH=$PATH:/mingw64/bin
 
-mv SDL2-2.28.5 src
+mv SDL2-2.32.10 src
 mkdir build dest
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=$X_WORK_DIR/dest --disable-shared
+cmake \
+"-DCMAKE_BUILD_TYPE=Release" \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DSDL_SHARED=OFF" \
+-G Ninja $X_WORK_DIR/src
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
-make $X_MAKE_JOBS install
+ninja
+ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
 
-mv libogg-1.3.5 src
+mv libogg-1.3.6 src
 mkdir build
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=$X_WORK_DIR/dest --disable-shared
+cmake \
+"-DCMAKE_BUILD_TYPE=Release" \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DBUILD_SHARED_LIBS=OFF" \
+-G Ninja $X_WORK_DIR/src
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
-make $X_MAKE_JOBS install
+ninja
+ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
 
@@ -38,41 +52,60 @@ mv libvorbis-1.3.7 src
 mkdir build
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=$X_WORK_DIR/dest --disable-shared
+# Set `CMAKE_POLICY_VERSION_MINIMUM` to work around https://github.com/StephanTLavavej/mingw-distro/issues/113
+cmake \
+"-DCMAKE_BUILD_TYPE=Release" \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DCMAKE_POLICY_VERSION_MINIMUM=4.2.1" \
+"-DBUILD_SHARED_LIBS=OFF" \
+-G Ninja $X_WORK_DIR/src
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
-make $X_MAKE_JOBS install
+ninja
+ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
 
-mv SDL2_mixer-2.6.3 src
+mv SDL2_mixer-2.8.1 src
 mkdir build
 cd build
 
-../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
---prefix=$X_WORK_DIR/dest --disable-shared
+# Disable usage of unavailable libraries.
+cmake \
+"-DCMAKE_BUILD_TYPE=Release" \
+"-DCMAKE_C_FLAGS=-s -O3" \
+"-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DBUILD_SHARED_LIBS=OFF" \
+"-DSDL2MIXER_MIDI_FLUIDSYNTH=OFF" \
+"-DSDL2MIXER_MOD=OFF" \
+"-DSDL2MIXER_OPUS=OFF" \
+"-DSDL2MIXER_WAVPACK=OFF" \
+-G Ninja $X_WORK_DIR/src
 
-# The `-I` options work around bugs in the build system: https://github.com/StephanTLavavej/mingw-distro/issues/98
-make $X_MAKE_JOBS all "CFLAGS=-s -O3 -I$X_WORK_DIR/src/include -I$X_WORK_DIR/src/src -I$X_WORK_DIR/src/src/codecs"
-make $X_MAKE_JOBS install
+ninja
+ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
 
-mv vorbis-tools-1.4.2 src
+mv vorbis-tools-1.4.3 src
 mkdir build
 cd build
 
+# vorbis-tools currently doesn't support CMake: https://github.com/StephanTLavavej/mingw-distro/issues/114
 ../src/configure --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 \
 --prefix=$X_WORK_DIR/dest --disable-nls
 
-make $X_MAKE_JOBS all "CFLAGS=-s -O3"
+# Compile with -std=gnu17 to work around https://github.com/StephanTLavavej/mingw-distro/issues/116
+make $X_MAKE_JOBS all "CFLAGS=-s -O3 -std=gnu17"
 make $X_MAKE_JOBS install
+
 cd $X_WORK_DIR
 rm -rf build src
 
 mv dest SDL+libogg+libvorbis+SDL_mixer+vorbis-tools
 cd SDL+libogg+libvorbis+SDL_mixer+vorbis-tools
-rm -rf bin/sdl2-config lib/cmake lib/pkgconfig lib/*.la share
+rm -rf bin/sdl2-config lib/cmake lib/pkgconfig share
 
 7z -mx0 a ../SDL+libogg+libvorbis+SDL_mixer+vorbis-tools.7z *
