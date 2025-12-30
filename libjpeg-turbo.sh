63b82b6b4cf1fa9b05d8a3189d91ed5d615dade4
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:25:04 -0800

    libjpeg-turbo 3.1.3.
    Compile with `-Wno-stringop-overflow` to silence false positives.
    
    We no longer need to set `CMAKE_ASM_NASM_FLAGS` and `CMAKE_ASM_NASM_OBJECT_FORMAT`, as they're now correctly detected.
    
    Disable `WITH_TESTS` for a slightly faster build.

--- libjpeg-turbo.sh
+++ libjpeg-turbo.sh
@@ -2,28 +2,32 @@
 
 source ./0_append_distro_path.sh
 
-untar_file libjpeg-turbo-3.0.1.tar
+untar_file libjpeg-turbo-3.1.3.tar
 
 cd $X_WORK_DIR
-mv libjpeg-turbo-3.0.1 src
+mv libjpeg-turbo-3.1.3 src
 mkdir build dest
 cd build
 
+export PATH=$PATH:/mingw64/bin
+# Compile with `-Wno-stringop-overflow` to silence false positives. See:
+# https://github.com/libjpeg-turbo/libjpeg-turbo/issues/722
+# https://github.com/libjpeg-turbo/libjpeg-turbo/issues/796
 cmake \
-"-DCMAKE_ASM_NASM_FLAGS=-DWIN64" \
-"-DCMAKE_ASM_NASM_OBJECT_FORMAT=win64" \
 "-DCMAKE_BUILD_TYPE=Release" \
-"-DCMAKE_C_FLAGS=-s -O3 -DTWO_FILE_COMMANDLINE" \
+"-DCMAKE_C_FLAGS=-s -O3 -DTWO_FILE_COMMANDLINE -Wno-stringop-overflow" \
 "-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
 "-DENABLE_SHARED=OFF" \
+"-DWITH_TESTS=OFF" \
 -G Ninja $X_WORK_DIR/src
 
 ninja
 ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
-mv dest libjpeg-turbo-3.0.1
-cd libjpeg-turbo-3.0.1
+mv dest libjpeg-turbo-3.1.3
+cd libjpeg-turbo-3.1.3
 rm -rf bin/cjpeg.exe bin/djpeg.exe bin/rdjpgcom.exe bin/tjbench.exe bin/wrjpgcom.exe lib/cmake lib/pkgconfig share
 
-7z -mx0 a ../libjpeg-turbo-3.0.1.7z *
+7z -mx0 a ../libjpeg-turbo-3.1.3.7z *
