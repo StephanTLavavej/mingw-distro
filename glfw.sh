45f95c1c6a4a42a3e76347fd41f79bf95489cdf3
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:12:35 -0800

    GLFW 3.4.
    We no longer need to set `WIN32`.

--- glfw.sh
+++ glfw.sh
@@ -2,18 +2,18 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glfw-3.3.9.tar
+untar_file glfw-3.4.tar
 
 cd $X_WORK_DIR
-mv glfw-3.3.9 src
+mv glfw-3.4 src
 mkdir build dest
 cd build
 
+export PATH=$PATH:/mingw64/bin
 cmake \
 "-DCMAKE_BUILD_TYPE=Release" \
 "-DCMAKE_C_FLAGS=-s -O3" \
 "-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
-"-DWIN32=ON" \
 "-DGLFW_BUILD_DOCS=OFF" \
 "-DGLFW_BUILD_EXAMPLES=OFF" \
 "-DGLFW_BUILD_TESTS=OFF" \
@@ -21,10 +21,11 @@ cmake \
 
 ninja
 ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
-mv dest glfw-3.3.9
-cd glfw-3.3.9
+mv dest glfw-3.4
+cd glfw-3.4
 rm -rf lib/cmake lib/pkgconfig
 
-7z -mx0 a ../glfw-3.3.9.7z *
+7z -mx0 a ../glfw-3.4.7z *
