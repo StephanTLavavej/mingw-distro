4bcce1878719db1c719a84bd01b82d8b17e8363c
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:00:15 -0800

    glbinding 3.5.0.
    We no longer need to use Boost.Thread, or compile with `-DSYSTEM_WINDOWS`.
    
    We no longer need to set OPTION_BUILD_TESTS to OFF, as that's the default.

--- glbinding.sh
+++ glbinding.sh
@@ -2,32 +2,31 @@
 
 source ./0_append_distro_path.sh
 
-untar_file glbinding-3.3.0.tar
+untar_file glbinding-3.5.0.tar
 
 cd $X_WORK_DIR
-mv glbinding-3.3.0 src
+mv glbinding-3.5.0 src
 mkdir build dest
 cd build
 
+export PATH=$PATH:/mingw64/bin
 cmake \
-"-DBOOST_ROOT=$X_DISTRO_ROOT" \
-"-DBUILD_SHARED_LIBS=OFF" \
 "-DCMAKE_BUILD_TYPE=Release" \
-"-DCMAKE_CXX_FLAGS=-DBOOST_THREAD_VERSION=4 -s -O3 -DSYSTEM_WINDOWS" \
+"-DCMAKE_CXX_FLAGS=-s -O3" \
 "-DCMAKE_INSTALL_PREFIX=$X_WORK_DIR/dest" \
+"-DBUILD_SHARED_LIBS=OFF" \
 "-DOPTION_BUILD_EXAMPLES=OFF" \
-"-DOPTION_BUILD_TESTS=OFF" \
 "-DOPTION_BUILD_TOOLS=OFF" \
-"-DOPTION_BUILD_WITH_BOOST_THREAD=ON" \
 -G Ninja $X_WORK_DIR/src
 
 ninja
 ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
-mv dest glbinding-3.3.0
-cd glbinding-3.3.0
+mv dest glbinding-3.5.0
+cd glbinding-3.5.0
 rm -rf cmake AUTHORS glbinding-config.cmake README.md VERSION
 mv LICENSE include/glbinding
 
-7z -mx0 a ../glbinding-3.3.0.7z *
+7z -mx0 a ../glbinding-3.5.0.7z *
