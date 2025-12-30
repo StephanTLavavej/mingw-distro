dcbbf8b513b94a23d9a1ca43a99da1b656534fe3
Stephan T. Lavavej <stl@nuwen.net>
2025-12-30 04:47:49 -0800

    zstd 1.5.7.
    We now have fewer files and directories to cleanup.

--- zstd.sh
+++ zstd.sh
@@ -4,13 +4,14 @@ source ./0_append_distro_path.sh
 
 # Work around https://github.com/msys2/MSYS2-packages/issues/1216 by excluding the directory
 # that contains the affected symlink. We don't need it, as ZSTD_BUILD_TESTS defaults to OFF.
-untar_file zstd-1.5.5.tar --exclude=zstd-1.5.5/tests
+untar_file zstd-1.5.7.tar --exclude=zstd-1.5.7/tests
 
 cd $X_WORK_DIR
-mv zstd-1.5.5 src
+mv zstd-1.5.7 src
 mkdir build dest
 cd build
 
+export PATH=$PATH:/mingw64/bin
 cmake \
 "-DCMAKE_BUILD_TYPE=Release" \
 "-DCMAKE_C_FLAGS=-s -O3" \
@@ -20,11 +21,11 @@ cmake \
 
 ninja
 ninja install
+
 cd $X_WORK_DIR
 rm -rf build src
-mv dest zstd-1.5.5
-cd zstd-1.5.5
-rm -rf bin/zstdgrep bin/zstdless lib/cmake lib/pkgconfig share
-for i in bin/unzstd bin/zstdcat bin/zstdmt; do mv $i $i.exe; done
+mv dest zstd-1.5.7
+cd zstd-1.5.7
+rm -rf lib/cmake lib/pkgconfig
 
-7z -mx0 a ../zstd-1.5.5.7z *
+7z -mx0 a ../zstd-1.5.7.7z *
